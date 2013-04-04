89bf55f214a66ebeca94c74bb26edfbfd935d2ca
teamalpha5441 <teamalpha5441@gmail.com>
2013-04-05 00:21:57 +0200

    Update

--- src/me/goa456/pokemon/Main.java
+++ src/me/goa456/pokemon/Main.java
@@ -1,40 +1,9 @@
-
 package me.goa456.pokemon;
 
-import jgame.*;
-import jgame.platform.*;
-/** Minimal shooter illustrating Eclipse usage. */
-public class Main extends StdGame {
+public class Main {
 	
 	public static void main(String[]args) {
-		Main game = new Main();
-	}
-	
-	public Main() {
-		initEngine(640, 480);
-	}
-
-	@Override
-	public void initCanvas() {
-		setCanvasSettings(
-			20,  // width of the canvas in tiles
-			15,  // height of the canvas in tiles
-			16,  // width of one tile
-			16,  // height of one tile
-			     //    (note: total size = 20*16=320  x  15*16=240)
-			null,// foreground colour -> use default colour white
-			null,// background colour -> use default colour black
-			null // standard font -> use default font
-		);
-	}
-	
-	public void initGame() {
-		// We can set the frame rate, load graphics, etc, at this point. 
-		// (note that we can also do any of these later on if we wish)
-		setFrameRate(
-			35,// 35 = frame rate, 35 frames per second
-			2  //  2 = frame skip, skip at most 2 frames before displaying
-			   //      a frame again
-		);
+		System.out.println("Loading Pokemon by goa456 / teamalpha5441");
+		//TODO Add the fuc*ing game
 	}
 }
